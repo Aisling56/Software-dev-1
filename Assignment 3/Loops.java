de69bdd6d244b8cd69cec72ff2a0730e3f54e6df
Aisling56 <aisling.garry1@marist.edu>
2026-09-24 18:35:20 -0400

    Add Loops.java to print each character of a string
    Implemented a Java program to take user input and print each character of the input string on a new line. Included comments explaining the logic and discovery process.

---
+++ Assignment 3/Loops.java
@@ -0,0 +1,31 @@
+/*
+challenge file!
+this one isn't too tough but it does require a little bit of writing and some googling
+figure out how to take a string from the user
+then print back out every individual letter one letter per line
+I also am requiring a small write up: explain your discovery to me.
+how did you figure out how to do this? can you translate your code into simple terms? 
+you need to explain why you picked your for loop conditional and what's doing the work with the string
+!!!!!!
+Look into things like charAt- DO NOT NAME YOUR VARIABLE "REVERSED". If you do, automatic 0 points!!!!!!
+!!!!!!!
+if you're confused reach out!
+ */
+
+
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a word:");
+    String var = sc.nextLine();
+
+    for (int i = 0; i < var.length(); i++){
+        System.out.println(var.charAt(i));
+    
+    }
+    sc.close();
+    }
+}
+// I figured out how to do this by going to google for some guidance. To translate, my code is taking input from the user that is in the form of a string. We start at i=0 (the first letter of the word), and we stop once we hit the number of characters the entered word has. And we are going forward throguh the word, so I used i++. I used the charAt(i) to get the character at each position i. This allows the terminal to print each letter of the word on a new line.  
